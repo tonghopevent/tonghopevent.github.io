bbd806f18a7c1bb09e554bb26fc26837148792d9
ngxson <thichthat@gmail.com>
2019-06-12 22:46:04 +0200

    v4

---
+++ data/precache-manifest.f4f2942e6e807bae5d81016b46e9b5f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54a086fe821810f37021c13dc63d5c87",
+    "url": "/data/index.html"
+  },
+  {
+    "revision": "7dc6e5fbdb29817a4d33",
+    "url": "/data/static/css/main.83bd8ac9.chunk.css"
+  },
+  {
+    "revision": "cccc04725a11de6b3132",
+    "url": "/data/static/js/2.32f14ad1.chunk.js"
+  },
+  {
+    "revision": "7dc6e5fbdb29817a4d33",
+    "url": "/data/static/js/main.d6f981c4.chunk.js"
+  },
+  {
+    "revision": "66c40181141e8dae0649",
+    "url": "/data/static/js/runtime~main.b2425654.js"
+  }
+]);
