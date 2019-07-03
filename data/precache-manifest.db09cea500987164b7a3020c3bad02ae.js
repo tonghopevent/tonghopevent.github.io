cd1f7541fb4db2c86fc6cffd0fb2235c21b4ea7f
ngxson <thichthat@gmail.com>
2019-07-04 01:30:38 +0200

    update to v4.3

---
+++ data/precache-manifest.db09cea500987164b7a3020c3bad02ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e9f1904ecca91a150bf81a2d55d0dd9",
+    "url": "/data/index.html"
+  },
+  {
+    "revision": "c87a1734818bc5ac2197",
+    "url": "/data/static/css/main.68dc20f5.chunk.css"
+  },
+  {
+    "revision": "4745efcecd086d867848",
+    "url": "/data/static/js/2.ec594c8d.chunk.js"
+  },
+  {
+    "revision": "c87a1734818bc5ac2197",
+    "url": "/data/static/js/main.58abfc39.chunk.js"
+  },
+  {
+    "revision": "66c40181141e8dae0649",
+    "url": "/data/static/js/runtime~main.b2425654.js"
+  }
+]);
--- data/service-worker.js
+++ data/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/data/precache-manifest.d8e6f312e83c9b86ee7fc635a1d0fbfc.js"
+  "/data/precache-manifest.db09cea500987164b7a3020c3bad02ae.js"
 );
 
 self.addEventListener('message', (event) => {
